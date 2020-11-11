9f9968eb8fbee334e636f6976eb4bd5ff976399a
Anthony Bowen <bowen.anth@gmail.com>
2020-11-11 15:06:01 -0800

    added array code to js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const missed = (0);
 const overlay = document.getElementById('overlay');
 const start = document.querySelectorAll('.btn__reset');
 
+const phrases = ["beat around the bush", "a dime a dozen", "life's a box of chocolates", "cheater's never prosper", "bite the bullet"];
+
+
 start.forEach(el => el.addEventListener('click', event => {
     console.log('test click');
     overlay.style.display === 'none';
